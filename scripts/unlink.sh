055c2f589feea2d57f70a9ad891892caeda72d8c
kevinslin <kevin@thence.io>
2020-11-07 14:36:32 -0800

    chore: update deps

--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -1,5 +1,9 @@
-VERSION=0.12.0
+VERSION_MUME=0.13.0
+VERSION_SERVER="0.14.2-alpha.1"
 
 yarn unlink @dendronhq/mume
-echo "installing $VERSION"
-yarn add --force @dendronhq/mume@$VERSION
+yarn unlink @dendronhq/engine-server
+echo "installing $VERSION_MUME"
+yarn add --force @dendronhq/mume@$VERSION_MUME
+echo "installing $VERSION_MUME"
+yarn add --force @dendronhq/engine-server@$VERSION_SERVER
