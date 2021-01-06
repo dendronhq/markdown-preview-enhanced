4e005cbb9b5eed0d50651fd2d35640ba9152d4f5
kevinslin <kevin@thence.io>
2021-01-06 08:08:41 -0800

    update deps

---
+++ scripts/installAll.sh
@@ -0,0 +1,8 @@
+cd ../dendron-mume/scripts
+./unlink.sh
+cd ..
+git add .
+git commit -m "update dependencies"
+./scripts/bump.sh
+
+cd dendron-markdown-preview-enhanced
--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -1,9 +1,9 @@
-VERSION_MUME=0.13.5
-VERSION_SERVER="0.22.2-alpha.0"
+MUME_VERSION=`cat ../dendron-mume/package.json | jq -r '.["version"]'`
+ENGINE_VERSION=`cat ../dendron/meta.json | jq -r '.["@dendronhq/engine-server"]'`
 
 yarn unlink @dendronhq/mume
 yarn unlink @dendronhq/engine-server
-echo "installing $VERSION_MUME"
+echo "installing mume $MUME_VERSION"
 yarn add --force @dendronhq/mume@$VERSION_MUME
-echo "installing $VERSION_MUME"
+echo "installing engine $ENGINE_VERSION"
 yarn add --force @dendronhq/engine-server@$VERSION_SERVER
