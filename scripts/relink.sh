0d6a019f6164680b323114a90b299bfb4bd3ba54
kevinslin <kevin@thence.io>
2021-03-01 13:57:40 -0800

    bump deps

--- scripts/relink.sh
+++ scripts/relink.sh
@@ -3,7 +3,4 @@ ENGINE_VERSION=`cat ../dendron/meta.json | jq -r '.["@dendronhq/engine-server"]'
 
 yarn unlink @dendronhq/mume
 yarn unlink @dendronhq/engine-server
-echo "installing mume $MUME_VERSION"
-yarn add --force @dendronhq/mume@$MUME_VERSION
-echo "installing engine $ENGINE_VERSION"
-yarn add --force @dendronhq/engine-server@$ENGINE_VERSION
+yarn add --force @dendronhq/mume@$MUME_VERSION @dendronhq/engine-server@$ENGINE_VERSION
